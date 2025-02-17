ac3f3483f6c1a5a3a6fa3a9516e30a8703bc2fdf
VictorRuiz <victorg.ruizl@ext.cemex.com>
2025-02-17 13:21:22 -0600

    add search mat input

--- supplier-portal/src/app/main-screen/navbar/navbar.component.ts
+++ supplier-portal/src/app/main-screen/navbar/navbar.component.ts
@@ -1,30 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatInputModule, MatTabsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-  listItems: any[] = [
-    { id: 1, name: 'Nuevos productos' },
-    { id: 2, name: 'Pollo' },
-    { id: 3, name: 'Cerdo' },
-    { id: 4, name: 'Res' },
-    { id: 5, name: 'Papas y otros' },
-    { id: 6, name: 'Aceites y mantecas' },
-    { id: 7, name: 'Mariscos' },
-    { id: 8, name: 'Salsas' },
-    { id: 9, name: 'Aderezos' },
-  ];
+export class NavbarComponent implements OnInit{
+  categories: any[] = [];
 
   searchKeyword: string = ''
 
   constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    // this.productService.products$.subscribe((products) => {
+    //   this.categories = new Set(products.map((product) => product.categoryId))
+    // })
+  }
 
   filterProducts(): void {
     this.productService.filterProducts(this.searchKeyword);
